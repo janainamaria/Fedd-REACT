69f64d4e7004dacb59400adb576265fa93243007
janaina maria <janainamariambm@gmail.com>
2022-06-10 15:19:29 -0300

    Inicio do sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 
 import  './global.css'
+import styles from './App.module.css'
 import { Header } from './components/Header';
 
 export function App() {
@@ -7,6 +8,21 @@ export function App() {
   return (
    <div>
      <Header/>
+     <div className={styles.wrapper}>
+        <aside>
+          Sidebar
+        </aside>
+        <main>
+          <Post
+          author='Diego moura'
+          content=' Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quod eaque inventore, suscipit quaerat quidem ex repellat cumque officia, eius aspernatur consequuntur quisquam, quam nostrum maiores. Harum cumque quae quas eius?'
+          />
+          <Post
+          author='Ana felix'
+          content=' Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quod eaque inventore, suscipit quaerat quidem ex repellat cumque officia, eius aspernatur consequuntur quisquam, quam nostrum maiores. Harum cumque quae quas eius?'
+          />
+        </main>
+     </div>
    </div>
       
   )
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,11 @@
 import styles from './Header.module.css'
+import logo from '../assets/ignite-logo.svg'
 
 
 export function Header(){
   return(
     <header className={styles.header}>
-    <strong>Feed</strong>
+    <img src={logo} alt="logotipo" />
     </header>
   );
 }
